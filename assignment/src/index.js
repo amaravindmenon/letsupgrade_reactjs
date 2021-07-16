7a4bf87acf8480f619552883d30512daa68564fc
amaravindmenon <amaravindmenon@gmail.com>
2021-07-16 11:45:36 +0530

    second commit

---
+++ assignment/src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Hello() {
+  return (
+    <h1> hello world </h1>
+  )
+}
+
+
+ReactDOM.render(<Hello />, document.getElementById("root"))
